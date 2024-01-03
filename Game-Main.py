abf4cf36ed40f992f00a6c686f71cd86b4e4033c
Aaron Johal <aaronjohal9@gmail.com>
2024-01-02 21:23:03 -0800

    Updates to guessing mechanics + note clarifiations

--- Game-Main.py
+++ Game-Main.py
@@ -1,14 +1,27 @@
-word = "coding"
+word = "coding"  #Word for player to guess
 
-allowed_errors = 8
+allowed_errors = 8 
 guesses = []
 done = False
 
 while not done:
     for letter in word:
-        if letter.lower() in guesses:
+        if letter.lower() in guesses: #Ignores case sensitivity
             print(letter, end=" ")
 
         else:
             print("_", end = " ")
     print("")
+
+
+    guess = input(f"Allowed Errors Left {allowed_errors}, Next Guess?:") #Notifys player how many guess they have
+    guesses.append(guess.lower())
+    if guess.lower () not in word.lower():
+        allowed_errors -= 1 #subtract amount of guesses left
+        if allowed_errors == 0: #Amount of guesses remain
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
